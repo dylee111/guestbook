e1cad417d55a086b431a6182205f0e97aca7f07d
dylee <dylee0568@gmail.com>
2021-09-01 20:14:05 +0900

    2021/09/01

--- src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -1,19 +1,54 @@
 package org.zerock.guestbook.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.service.GuestbookService;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor // 자동 주입을 위한 Annotation
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
-    public String list() {
 
-        log.info("list.......");
-        return "/guestbook/list";
+    private final GuestbookService service;
+
+    @GetMapping({"/"})
+    public String index() {
+        return "redirect:/guestbook/list";
     }
+
+    // void : 요청된 url이 리소스로 바로 감.
+    @GetMapping({"/list"})
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+
+        log.info("list......." + pageRequestDTO);
+        model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("RegisterPost...");
+        // 새로 추가된 엔티티 번호.
+        Long gno = service.register(dto);
+        // addFlashAttribute : 1회성으로 데이터를 전달하는 용도로 사용.
+        redirectAttributes.addFlashAttribute("msg",gno);
+
+        return "redirect:/guestbook/list";
+    }
+
+
 }
